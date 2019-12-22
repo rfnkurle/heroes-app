dcdf32b132debab8fc8b836f67e319b0c65f28a3
robiwankenobi <rfnkurle@gmail.com>
2019-12-22 12:54:13 -0700

    added onclick binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero' 
+import { Hero } from '../hero'; 
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  
+  heroes = HEROES; 
+  //names a property that can be used in HTML, set to HEROES array
+  //heroes holds the mock heroes list from the HeroesComponent class, the mock heroes list.
+  //hero holds the current hero object for each iteration through the list.
+  
+  
+  selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
+  
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
   }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,18 @@
+//import Hero class to be used in array below
+import { Hero } from './hero';
+
+
+//Define a HEROES constant as an array of ten heroes and export it. 
+//const HEROES specifically defined as a Hero class array
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
