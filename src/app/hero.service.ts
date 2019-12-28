94967ab9644bd1aa0c682266c30a4ba3e8c8fd8e
robiwankenobi <rfnkurle@gmail.com>
2019-12-28 13:21:16 -0700

    added HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'//single shared instance of service
 })
+
 export class HeroService {
 
+  
+
+
   constructor() { }
+
+  getHeroes():Hero[]{
+    return HEROES;
+  }
+
 }
